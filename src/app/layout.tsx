97211bd3fbb7154a340c15af1f5285c03d2c7237
Zain sheikh <sheikhzain95@gmail.com>
2026-02-09 16:03:03 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import SnowCanvas from "@/components/Snow";
+// import SnowCanvas from "@/components/Snow";
 import { Analytics } from "@vercel/analytics/next";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
@@ -233,7 +233,7 @@ export default function RootLayout({
             __html: JSON.stringify(storeSchema),
           }}
         />
-        <SnowCanvas />
+        {/*<SnowCanvas />*/}
         <Header />
         <main className="flex-1">{children}</main>
 
